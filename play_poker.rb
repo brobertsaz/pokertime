1bc1a0b154b8c70db59ed8dfb8601fce0997b56a
Bob Roberts <bob.roberts@rever.co>
2021-10-21 15:59:02 -0500

    working on figuring out how to compare the 2 hands

--- play_poker.rb
+++ play_poker.rb
@@ -1,3 +1,5 @@
+require './hand.rb'
+
 File.open("data/poker.txt").read.each_line do |line|
    length = line.size
    split_count = length/2
@@ -5,4 +7,9 @@ File.open("data/poker.txt").read.each_line do |line|
    player2 = line[split_count..-1].strip
    puts "Player 1: " + player1
    puts "Player 2: " + player2
+   if Hand.new(player1) > Hand.new(player2)
+      puts 'Player 1 won'
+   else
+      puts 'Player 2 won'
+   end
 end
